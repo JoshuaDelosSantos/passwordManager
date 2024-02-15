c6b941c0f929d4bdac9f272ce1cba2ca0b7ffc8c
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2024-02-15 10:36:53 +1000

    Change __init__ variable from password to salt and hash_code.

--- user.py
+++ user.py
@@ -4,11 +4,12 @@ User class that initialises a user's username and password.
 
 
 class User:
-    def __init__(self, user_name, password):
+    def __init__(self, user_name, salt, hash_code):
         """Initialise user instance."""
         self.user_name = user_name
-        self.password = password
+        self.salt = salt
+        self.hash_code = hash_code
 
     def __str__(self):
         """Return string representation of user_name and user password."""
-        return f"{self.user_name} : {self.password}"
+        return f"{self.user_name},{self.salt},{self.hash_code}"
